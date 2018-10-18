ebad45a5df9f7593db692354b3d9cabf4eb9f36d
Terry Brown <ptbrown514@gmail.com>
2018-10-17 23:05:41 -0500

    display of numbers when clicked

---
+++ calc.js
@@ -0,0 +1,73 @@
+var myInput = document.getElementById(input);
+var myAnswer = document.getElementById(answer);
+
+function clearOnce(){
+        myInput.value = myInput.value.substring(0, myInput.value.length - 1);
+    }
+
+    function getNumberZero() {
+        var zero = document.getElementById("0").value;
+        console.log(document.getElementById("0").value);
+        var input = document.getElementById("input");
+        input.value = zero;
+        }
+    function getNumberOne() {
+        var one = document.getElementById("1").value;
+        console.log(document.getElementById("1").value);
+        var input = document.getElementById("input");
+        input.value = one;
+        document.classList.add("input::placeholder")
+    }
+    function getNumberTwo() {
+        var two = document.getElementById("2").value;
+        console.log(document.getElementById("2").value);
+        var input = document.getElementById("input");
+        input.value = two;
+    }
+    function getNumberThree() {
+        var three = document.getElementById("3").value;
+        console.log(document.getElementById("3").value);
+        var input = document.getElementById("input");
+        input.value = three;
+    }
+    function getNumberFour() {
+        var four = document.getElementById("4").value;
+        console.log(document.getElementById("4").value);
+        var input = document.getElementById("input");
+        input.value = four;
+    }
+    function getNumberFive() {
+        var five = document.getElementById("5").value;
+        console.log(document.getElementById("5").value);
+        var input = document.getElementById("input");
+        input.value = five;
+    }
+    function getNumberSix() {
+        var six = document.getElementById("6").value;
+        console.log(document.getElementById("6").value);
+        var input = document.getElementById("input");
+        input.value = six;
+    }
+    function getNumberSeven() {
+        var seven = document.getElementById("7").value;
+        console.log(document.getElementById("7").value);
+        var input = document.getElementById("input");
+        input.value = seven;
+    }
+    function getNumberEight() {
+        var eight = document.getElementById("8").value;
+        console.log(document.getElementById("8").value);
+        var input = document.getElementById("input");
+        input.value = eight;
+    }
+    function getNumberNine() {
+        var nine = document.getElementById("9").value;
+        console.log(document.getElementById("9").value);
+        var input = document.getElementById("input");
+        input.value = nine;
+    }
+    
+    // function useOperandMultiply() {
+    //     var multiply = 
+        
+    // }
