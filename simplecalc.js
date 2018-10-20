b15efa3e4915d28100bda1b97f7d5faf69163bee
Terry Brown <ptbrown514@gmail.com>
2018-10-19 21:49:30 -0500

    made a list of past answers and clear button

--- simplecalc.js
+++ simplecalc.js
@@ -13,3 +13,16 @@ function compute() {
         document.getElementById("answer").value = one / two;
     }
 }
+
+function erase() {
+    var one = document.getElementById("inputOne");
+    var two = document.getElementById("inputTwo");
+    var answer = document.getElementById("answer");
+    var answerItems = document.getElementById("answerItems");
+    var pastAnswer = answer.value;
+    one.value = '';
+    two.value = '';
+    answer.value = '';
+    var answerList = answerItems.innerHTML;
+    answerItems.innerHTML = '<li style= "color: white">' + pastAnswer + '</li>' + answerList;
+}
